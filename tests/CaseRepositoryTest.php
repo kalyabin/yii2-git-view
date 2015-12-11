b86d26a417fec8d618b1f98644864f6386f128b8
Max Kalyabin <maksim@kalyabin.ru>
2015-12-11 22:03:20 +0300

    - more tests

--- tests/CaseRepositoryTest.php
+++ tests/CaseRepositoryTest.php
@@ -11,6 +11,7 @@ use VcsCommon\exception\CommonException;
 use VcsCommon\File;
 use Yii;
 use VcsCommon\Graph;
+use GitView\Diff;
 
 /**
  * Test repository
@@ -117,11 +118,77 @@ class CaseRepositoryTest extends PHPUnit_Framework_TestCase
      *
      * @param Commit[] $history
      * @depends testHistory
+     * @return Commit
      */
     public function testCommit(array $history)
     {
         $commit = reset($history);
-        $this->repository->getCommit($commit->getId());
+        return $this->repository->getCommit($commit->getId());
+    }
+
+    /**
+     * Tests commit variables
+     *
+     * @param GitWrapper $wrapper
+     * @param Commit $commit
+     * @return Commit
+     *
+     * @depends testCommit
+     */
+    public function testCommitVariables(Commit $commit)
+    {
+        $this->assertInstanceOf('DateTime', $commit->getDate());
+
+        /* @var $wrapper GitWrapper */
+        $wrapper = $this->repository->getWrapper();
+        $this->assertTrue($wrapper->checkIsSha1($commit->getId()));
+        $this->assertNotEmpty($commit->contributorName);
+        $this->assertNotEmpty($commit->contributorEmail);
+        $this->assertNotEmpty($commit->message);
+
+        return $commit;
+    }
+
+    /**
+     * Test commit diff
+     *
+     * @depends testCommitVariables
+     * @param Commit $commit
+     */
+    public function testCommitDiff(Commit $commit)
+    {
+        $diffs = $commit->getDiff();
+        $this->assertNotEmpty($diffs);
+        $this->assertContainsOnlyInstancesOf(Diff::className(), $diffs);
+        foreach ($diffs as $diff) {
+            /* @var $diff Diff */
+            $this->assertInternalType('string', $diff->getDescription());
+            $this->assertInternalType('string', $diff->getNewFilePath());
+            $this->assertNotEmpty($diff->getNewFilePath());
+            $this->assertNotEmpty($diff->getPreviousFilePath());
+            $this->assertContainsOnly('array', $diff->getLines());
+            foreach ($diff->getLines() as $diffKey => $lines) {
+                $this->assertInternalType('string', $diffKey);
+                $this->assertRegExp('#^@@[\s]\-([\d]+),?([\d]+)?[\s]\+([\d]+),?([\d]+)?[\s]@@#i', $diffKey);
+                $this->assertArrayHasKey('beginA', $lines);
+                $this->assertArrayHasKey('beginB', $lines);
+                $this->assertArrayHasKey('cntA', $lines);
+                $this->assertArrayHasKey('cntB', $lines);
+                $this->assertInternalType('integer', $lines['beginA']);
+                $this->assertInternalType('integer', $lines['beginB']);
+                $this->assertInternalType('integer', $lines['cntA']);
+                $this->assertInternalType('integer', $lines['cntB']);
+                $this->assertArrayHasKey('lines', $lines);
+                $this->assertInternalType('array', $lines['lines']);
+                $this->assertNotEmpty($lines['lines']);
+                $this->assertContainsOnly('string', $lines['lines']);
+                foreach ($lines['lines'] as $line) {
+                    if (!empty($line)) {
+                        $this->assertRegExp('#^([\s]|\+|\-){1}#i', $line);
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -221,6 +288,21 @@ class CaseRepositoryTest extends PHPUnit_Framework_TestCase
         $graph = $this->repository->getGraphHistory(10, 1);
         $this->assertNotEmpty($graph);
         $this->assertContainsOnlyInstancesOf(Graph::className(), $graph);
+
+        $hasOneOrMoreCommits = false;
+        foreach ($graph as $item) {
+            /* @var $item Graph */
+            $this->assertInternalType('boolean', $item->hasCommitPiece());
+            $hasOneOrMoreCommits = $hasOneOrMoreCommits || $item->hasCommitPiece();
+            $this->assertNotEmpty($item->getType());
+            $this->assertContainsOnly('string', $item->getType());
+            if ($item->hasCommitPiece()) {
+                $this->assertInstanceOf(Commit::className(), $item->getCommit());
+            }
+            else {
+                $this->assertNull($item->getCommit());
+            }
+        }
     }
 
     /**
