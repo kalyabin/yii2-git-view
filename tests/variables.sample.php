a7a593c9c42bb74b270e1d7d320dd8e54702142c
Max Kalyabin <maksim@kalyabin.ru>
2016-03-17 00:56:54 +0300

    - fix tests sample config

--- tests/variables.sample.php
+++ tests/variables.sample.php
@@ -21,6 +21,10 @@ return [
         ],
     ],
     'commit' => [
-        'id' => '1244f5a7409604e4027e9d97538f65d32767fb14',
+        'diff' => '1244f5a7409604e4027e9d97538f65d32767fb14',
+        'rawFile' => [
+            'commitId' => '95c5af88b6b83e66f67e09962d92a289d7916a18',
+            'file' => 'Directory.php'
+        ],
     ],
 ];
